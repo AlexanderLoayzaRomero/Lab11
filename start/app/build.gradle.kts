ca3047d4c6cc1f724a344477c15336b559235499
Alexander <alexander.loayza@tecsup.edu.pe>
2025-11-05 22:11:00 -0500

    Todo

--- start/app/build.gradle.kts
+++ start/app/build.gradle.kts
@@ -93,6 +93,9 @@ dependencies {
     //Test
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.43.2")
     androidTestImplementation("com.google.truth:truth:1.1.3")
